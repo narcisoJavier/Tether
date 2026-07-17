35cdf17d4930b105de5156970ef787a7fdb8727e
Renzo Javier <renzoj156@gmail.com>
2026-07-17 22:34:41 +0800

    build: configure release keystore and bump version to v0.4.0+2

--- lib/utils/app_version.dart
+++ lib/utils/app_version.dart
@@ -11,7 +11,7 @@ class AppVersion {
   AppVersion._();
 
   /// Compile-time fallback used when the platform package-info lookup fails.
-  static const String _fallbackVersion = '0.3.0';
+  static const String _fallbackVersion = '0.4.0';
 
   static String? _cached;
 
